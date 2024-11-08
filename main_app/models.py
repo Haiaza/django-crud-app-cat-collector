27db597607f0513e8a03c92c0a0d04a86ecf90e7
haiaza <ajames1342@gmail.com>
2024-11-07 19:42:19 -0500

    typo

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Cat(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000) #textfields should be considered with 1000's
+    age = models.IntegerField()
+
+    def __str__(self): # so we have a useful namw when we callupon the cat class
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -5,7 +5,26 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    return render(request, './home/home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+class Cat:
+    # Init is important!  Every class usees these attributes to intstantiate
+    def __init__(self, name, breed, description, age):
+        self.breed = breed
+        self.name = name
+        self.description = description
+        self.age = age
+
+        # Create a list of Cat instances
+cats = [
+    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
+    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
+
+def cat_index(request):
+    return render(request, 'cats/index.html',{'cats':cats})
