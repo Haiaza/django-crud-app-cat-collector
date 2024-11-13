c5fb226cbe12b5d71a593f4ed69a0b1b06519968
haiaza <ajames1342@gmail.com>
2024-11-12 20:18:57 -0500

    Views,Urls,Details,Update,Delete,

--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Cat
 # Import HttpResponse to send text-based responses
 from django.http import HttpResponse
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 # Create your views here.
 def home(request):
     return render(request, './home/home.html')
@@ -29,3 +29,21 @@ def about(request):
 def cat_index(request):
     cats = Cat.objects.all()
     return render(request, 'cats/index.html',{'cats':cats})
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+    success_url = '/cats/'
+
+class CatUpdate(UpdateView):
+    model = Cat
+    # Let's disallow the renaming of a cat by excluding the name field!
+    fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
